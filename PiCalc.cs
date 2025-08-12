4054df533283562cabb8b350cc8d0aa7b2ed087d
phragasm <149212307+phragasm@users.noreply.github.com>
2025-08-12 12:15:40 -0400

    Add files via upload
    This is a C# version of a calculator that calculates digits of Pi.

---
+++ PiCalc.cs
@@ -0,0 +1,32 @@
+﻿namespace FinalProject
+{
+    class PiCalculator
+    {
+        static void Main()
+        {
+            PiCalc();
+        }
+        static void PiCalc()
+        {
+            Console.WriteLine("Please enter a Positive Number:");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            double x = 0;
+            int y = 1;
+                if (userInput > 0)
+                {
+                    int count;
+                    for (count = 1; count < userInput; count += 2)
+                    {
+                        x = x + y * (1.0 /count);
+                        y = y * (-1);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Error, please enter a positive number.");
+                }
+                double Pi = x * 4;
+            Console.WriteLine(Pi);
+        }
+    }
+}
